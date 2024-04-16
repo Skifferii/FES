50b1fc943474d376313e5bea9ceb6c91240c8122
Skiferii <dolzhenkoandriy@gmail.com>
2024-04-16 13:32:40 +0200

    home work

---
+++ FrontEnd/Lesson10/code/exemple03/index.js
@@ -0,0 +1,17 @@
+// templare string (schablonnay stroka)
+
+const nameA = "SpiderMan"
+const strA = "Hallo " + nameA;
+console.log(strA);
+const strB = `Hello ${nameA}`;
+
+console.log(strB);
+console.log("----------------------------------------------------------");
+
+const myName =( str) =>`My name is ${str}`;
+console.log(myName("BatMan"));
+console.log("----------------------------------------------------------");
+
+const doubleF = ( name, strC ) => {
+   return  
+}
