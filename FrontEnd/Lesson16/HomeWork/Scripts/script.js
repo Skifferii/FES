5113a7d8509885f650f6236600d20c76ecd42a31
Skiferii <dolzhenkoandriy@gmail.com>
2024-04-25 18:20:00 +0200

    Alles

--- FrontEnd/Lesson16/HomeWork/Scripts/script.js
+++ FrontEnd/Lesson16/HomeWork/Scripts/script.js
@@ -3,15 +3,14 @@ const inputElement = document.querySelector('.input');
 const buttonElement = document.querySelector('button');
 let counter = 0;
 let intervalId;
-
 const timeHandler = () => {
     spanElement.innerText = counter;
     counter--;
     if (counter < 0) { 
         clearInterval(intervalId);
-        spanElement.innerText = "0 - Время вышло"; 
-                }
-
+        spanElement.innerText = "0 - Время вышло";
+        alert ("Время вышло") 
+        }
 };
 buttonElement.addEventListener('click', () => {
     counter = inputElement.value;
