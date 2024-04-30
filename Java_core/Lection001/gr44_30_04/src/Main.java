c002d19769f80531b87a1ef4a7f17da114c48ed3
Skiferii <dolzhenkoandriy@gmail.com>
2024-04-30 18:24:16 +0200

    Java

--- Java_core/Lection001/gr44_30_04/src/Main.java
+++ Java_core/Lection001/gr44_30_04/src/Main.java
@@ -1,6 +1,34 @@
 public class Main {
     public static void main(String[] args) {
+        int a=0;
+        Person person = new Person();
+        person.name = "John";
+        person.age = 30;
+        person.height = 180;
 
-        System.out.println("Hello world!");
+
+        Person person1 = new Person();
+        person1.name = "Kate";
+        person1.age = 25;
+        person1.height =170;
+
+        System.out.println(person.name);
+        System.out.println(person.age);
+        System.out.println(person.height);
+        person.canWalk();
+        System.out.println("_____________________________");
+
+        Person bill =new Person("Bill", 40,185);
+        System.out.println(bill.name);
+        System.out.println(bill.age);
+        System.out.println(bill.height);
+        bill.canWalk();
+        System.out.println("_____________________________");
+
+
+
+
+
+
     }
 }
---
+++ Java_core/Lection001/gr44_30_04/src/Person.java
@@ -0,0 +1,24 @@
+public class Person {
+    public String name;
+    public int age;
+    public int height;
+
+
+    public Person (){
+
+    }
+
+    public Person(String personName, int personAge, int personHeight){
+        name = personName;
+        age = personAge;
+        height = personHeight;
+
+    };
+    public void canWalk(){
+        System.out.println("a person can walk");
+
+    };
+
+
+
+}
