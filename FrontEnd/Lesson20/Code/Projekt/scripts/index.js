953a4976a813fe51b91c801f3abe829f31f2a93a
Skiferii <dolzhenkoandriy@gmail.com>
2024-04-30 17:27:40 +0200

    Alles drin

--- FrontEnd/Lesson20/Code/Projekt/scripts/index.js
+++ FrontEnd/Lesson20/Code/Projekt/scripts/index.js
@@ -1,16 +1,24 @@
 const button = document.querySelector(".button")
 const city = document.querySelector("#input_city")
+if (localStorage.getItem("city")){
+    // console.log (localStorage.getItem("city"));
+    recordNewcity();
+
+}
+// else {console.log("test")}
 
 button.addEventListener('click', recordNewcity);
 city.addEventListener('keypress', event => event.keyCode === 13 && recordNewcity());
 function recordNewcity()
-
 {
     let newcity;
     newcity = city.value.trim(); 
-     if (newcity) 
+    if (!city.value.trim() && localStorage.getItem("city")) {newcity=localStorage.getItem("city")
+}
+         if (newcity) 
      {  
-const url = "http://api.weatherapi.com/v1/current.json?key=ef3b4a40dd54446ea49160325242904&q="+city.value+"&aqi=no"  // name    
+        localStorage.setItem("city", newcity)
+const url = "http://api.weatherapi.com/v1/current.json?key=ef3b4a40dd54446ea49160325242904&q="+newcity+"&aqi=no"  // name    
 let dataResponse;
 const getData = async (api) => {
     const serverResponse = await fetch(api)
@@ -31,25 +39,22 @@ function dataWork(data){
     const month = monthNames[getData.getMonth()];
     let dateHeaderDiv = document.querySelector(".date");
     dateHeaderDiv.innerText = day+"."+ " " + month + " " + getData.getDate();
-     
-    
+        
     
     let city = document.querySelector(".city");  // Находим элемент с классом .city
     city.innerText = " " + data.location.name + " " + data.location.region;
-    // nameDiv.classList.add("city");  // Присваиваем класс для стилизации
+    // nameDiv.classList.add("city");  // Присваиваем класс для стилизации         
     
-       
     let imageDiv = document.querySelector(".image");
     let image = document.createElement("img");  // Создаем элемент изображения
     document.querySelector(".image").innerHTML = "";
     image.src = data.current.condition.icon;    // src є скороченням від "source" (джерело) і вказує шлях до зображення data.city                               
     // imageDiv.classList.add("cloudy-image"); // Присваиваем класс для стилизации (если требуется)
     imageDiv.appendChild(image); // Добавляем изображение внутрь элемента imageDiv         
-    
-
+        
     let cloudyDiv = document.querySelector("#clody")
     cloudyDiv.innerText = "" + data.current.condition.text;
-
+    
     let currentTempC = document.querySelector("#temp")
     currentTempC.innerText = "" + data.current.temp_c;
 
