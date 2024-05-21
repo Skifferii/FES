72034132e5bfbf84fa458c81a2411ff7a8a3f010
Skiferii <dolzhenkoandriy@gmail.com>
2024-05-21 08:21:53 +0200

    alles

--- Java_core/Lesson27/HomeWork_01/HomeWork_20240521_01/src/ListTasks.java
+++ Java_core/Lesson27/HomeWork_01/HomeWork_20240521_01/src/ListTasks.java
@@ -5,6 +5,7 @@ import java.util.Comparator;
 public class ListTasks {
 
     // Метод для объединения всех строк в одну
+
     public static String concatenateStrings(List<String> strings) {
         StringBuilder result = new StringBuilder();
         for (String s : strings) {
@@ -14,6 +15,7 @@ public class ListTasks {
     }
 
     // Метод для объединения строк по убыванию их длины
+
     public static String concatenateStringsByLength(List<String> strings) {
         strings.sort(Comparator.comparingInt(String::length).reversed());
         StringBuilder result = new StringBuilder();
@@ -25,16 +27,16 @@ public class ListTasks {
 
     // Метод для замены повторяющихся слов
     public static List<String> replaceDuplicates(List<String> strings, String word) {
-        boolean firstOccurrence = true;
-        for (int i = 0; i < strings.size(); i++) {
-            if (strings.get(i).equalsIgnoreCase(word)) {
-                if (firstOccurrence) {
-                    firstOccurrence = false;
-                } else {
-                    strings.set(i, "[Повтор] " + word);
+        boolean firstOccurrence = true; // Флаг для отслеживания первого вхождения слова
+        for (int i = 0; i < strings.size(); i++) { // Цикл для перебора всех элементов списка
+            if (strings.get(i).equalsIgnoreCase(word)) { // Проверка, равно ли текущее слово заданному слову (без учета регистра)
+                if (firstOccurrence) { // Если это первое вхождение
+                    firstOccurrence = false; // Устанавливаем флаг в false, чтобы отметить, что первое вхождение найдено
+                } else { // Если это не первое вхождение
+                    strings.set(i, "[Повтор] " + word); // Заменяем текущее слово на "[Повтор] " + слово
                 }
             }
         }
-        return strings;
+        return strings; // Возвращаем измененный список
     }
 }
